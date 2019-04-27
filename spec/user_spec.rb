c546616d9e99c401918f079b3e0f6a30df98dfa9
kohei.suzuki <3173010@stu.hus.ac.jp>
2019-04-27 18:30:55 +0000

    use before in nest

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -8,21 +8,28 @@ RSpec.describe User do
   describe "#greet" do 
     
     #beforeをつかってDRY。このとき、beforeとcontextの中でスコープが異なるため、インスタンス変数を使う。
+    #beforeをネストすると親から順に呼ばれる.
     before do 
       @params = {name:"たろう"}
     end
     
     # contextで条件別にグループ化をする。(12歳以下 or 13歳以上)
     context "12歳以下の場合" do 
+      before do 
+        @params.merge!(age:12)
+      end
       it "ひらがなで答えること" do 
-        user = User.new(@params.merge(age: 12))
+        user = User.new(@params)
         expect(user.greet).to eq "ぼくはたろうだよ。"
       end
     end
     
     context "13歳以上の場合" do 
+      before do 
+        @params.merge!(age:13)
+      end
       it "漢字で答えること" do 
-        user = User.new(@params.merge(age: 13))
+        user = User.new(@params)
         expect(user.greet).to eq "僕はたろうです。"
       end
     end
