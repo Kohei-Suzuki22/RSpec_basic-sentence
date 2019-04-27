594b65753b4a79b0e9e0df90f1fe8073795e9b4f
kohei.suzuki <3173010@stu.hus.ac.jp>
2019-04-27 18:26:17 +0000

    use before

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -4,22 +4,25 @@ require "spec_helper"
 
 # describeには describe User のように、文字列ではなくクラスを渡すこともできる。
 # describe "#greet"のように書くことで、「インスタンスメソッドのgreetメソッドをテストするよ」と意味づけ。
-
 RSpec.describe User do 
   describe "#greet" do 
     
-    # contextで条件別にグループ化をする。(12歳以下 or 13歳以上)
+    #beforeをつかってDRY。このとき、beforeとcontextの中でスコープが異なるため、インスタンス変数を使う。
+    before do 
+      @params = {name:"たろう"}
+    end
     
+    # contextで条件別にグループ化をする。(12歳以下 or 13歳以上)
     context "12歳以下の場合" do 
       it "ひらがなで答えること" do 
-        user = User.new(name: "たろう", age: 12)
+        user = User.new(@params.merge(age: 12))
         expect(user.greet).to eq "ぼくはたろうだよ。"
       end
     end
     
     context "13歳以上の場合" do 
       it "漢字で答えること" do 
-        user = User.new(name: "たろう", age: 13)
+        user = User.new(@params.merge(age: 13))
         expect(user.greet).to eq "僕はたろうです。"
       end
     end
