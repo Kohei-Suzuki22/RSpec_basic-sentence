c3e296fbd53d4d32cc2116e6b74f56369878b688
kohei.suzuki <3173010@stu.hus.ac.jp>
2019-04-27 18:20:06 +0000

    use context

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -2,17 +2,26 @@ require_relative "../lib/user"
 
 require "spec_helper"
 
+# describeには describe User のように、文字列ではなくクラスを渡すこともできる。
+# describe "#greet"のように書くことで、「インスタンスメソッドのgreetメソッドをテストするよ」と意味づけ。
+
 RSpec.describe User do 
   describe "#greet" do 
     
-    it "12歳以下の場合、ひらがなで答えること" do 
-      user = User.new(name:"たろう",age:12)
-      expect(user.greet).to eq "ぼくはたろうだよ。"
+    # contextで条件別にグループ化をする。(12歳以下 or 13歳以上)
+    
+    context "12歳以下の場合" do 
+      it "ひらがなで答えること" do 
+        user = User.new(name: "たろう", age: 12)
+        expect(user.greet).to eq "ぼくはたろうだよ。"
+      end
     end
     
-    it "13歳以上の場合、漢字で答えること" do 
-      user = User.new(name:"たろう",age:13)
-      expect(user.greet).to eq "僕はたろうです。"
+    context "13歳以上の場合" do 
+      it "漢字で答えること" do 
+        user = User.new(name: "たろう", age: 13)
+        expect(user.greet).to eq "僕はたろうです。"
+      end
     end
     
   end
