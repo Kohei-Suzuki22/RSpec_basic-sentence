9681bbe196206e98bff25c5fa5f9fe7c4f1c5f01
kohei.suzuki <3173010@stu.hus.ac.jp>
2019-04-28 02:57:43 +0000

    use let

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -36,3 +36,64 @@ RSpec.describe User do
     
   end
 end
+
+
+#let を使った書き換え。
+#let は遅延評価。= 必要になる瞬間まで呼び出されない。
+#let はexampleがことなる場合は異なるオブジェクトを返す。
+
+#let の利点
+  #1.letを使わずインスタンス変数を生成する場合は、typo(打ち間違えなど)するとnil　で初期化するが、
+  #  letはtypoするとエラーが発生するので、ミスに気づき対処しやすい。
+  #2.使われる瞬間に呼び出されるため、無駄な時間を削減出来る。
+
+#評価順番
+
+# 1. expect(user.greet).to → userを呼び出す。
+# 2. let(:user){User.new(params)} → paramsを呼び出す。
+# 3. let(:params){{name:"たろう", age: age}}　→　ageを呼び出す。
+# 4. let(:age){12} or let(:age){13} によってそれぞれ呼び出す。
+
+# 5. expect(user.greet).to が指定した値として評価される。
+
+RSpec.describe User do 
+  describe "#greet" do 
+    
+    #{name:"太郎"}　を　params として参照出来る。
+    
+    let(:params){{name: "たろう", age: age}}
+    let(:user){User.new(params)}
+    
+    #上の書き換え
+    
+    # let(:params) do 
+    #   hash = {}
+    #   hash[:name] = "たろう"
+    #   hash
+    # end
+    
+    context "12歳以下の場合" do 
+      # before do 
+      #   params.merge!(age:12)
+      # end
+      let(:age){12}
+      
+      it "ひらがなで答えること" do 
+        # user = User.new(params)
+        expect(user.greet).to eq "ぼくはたろうだよ。"
+      end
+    end
+    
+    context "13歳以上の場合" do 
+      # before do 
+      #   params.merge!(age:13)
+      # end
+      let(:age){13}
+      
+      it "漢字で答えること" do 
+        # user = User.new(params)
+        expect(user.greet).to eq "僕はたろうです。"
+      end
+    end
+  end
+end
